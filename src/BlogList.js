66651081770d51e0492882b21e1c7e75ad4a9e6a
marlar-tz <marlarhal55@gmail.com>
2025-10-03 11:31:27 +0800

    Added function as a props

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,6 +2,7 @@ const BlogList = (props) => {
 
     const blogs = props.blogs;
     const title = props.title;
+    const handleDelete = props.handleDelete;
 
     return (
         <div className="blog-list">
@@ -10,6 +11,7 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written byt {blog.author}</p>
+                    <button onClick={()=> handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
