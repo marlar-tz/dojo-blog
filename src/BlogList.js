6cd473ba9d0449de7f5a17bcb5166457accba9fb
marlar-tz <marlarhal55@gmail.com>
2025-10-04 13:13:01 +0800

    Fetched data from json using useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,7 +2,6 @@ const BlogList = (props) => {
 
     const blogs = props.blogs;
     const title = props.title;
-    const handleDelete = props.handleDelete;
 
     return (
         <div className="blog-list">
@@ -11,7 +10,6 @@ const BlogList = (props) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>Written byt {blog.author}</p>
-                    <button onClick={()=> handleDelete(blog.id)}>delete blog</button>
                 </div>
             ))}
         </div>
