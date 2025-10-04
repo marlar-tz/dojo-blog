9533543d2b04f00a6888e82b452798200b293064
marlar-tz <marlarhal55@gmail.com>
2025-10-04 19:08:27 +0800

    Added Error Message

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,36 @@ import { useState , useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
+    const [blogs, setBlogs] = useState(null);
+    const [isPending , setIsPending] = useState(true);
 
+    const [error , setError] = useState(null);
     useEffect(()=>{
-        fetch('http://localhost:8080/blogs')
+        setTimeout(()=>{
+            fetch('http://localhost:8080/blogss')
         .then(res =>{
+            if(!res.ok){
+                throw Error('could not fetch the data for that resources');
+            }
+           
             return res.json();
         })
         .then((data)=>{
-            setBlogs(data)
+            setBlogs(data);
+            setIsPending(false);
+            setError(null);
         })
+        .catch(err => {
+            setError(err.message);
+            setIsPending(false);
+        })
+        },1000);
     },[])
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            { isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All blogs!" />}
             {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario' )} title="Mario's blogs!" /> */}
             {/* <button onClick={()=>{setName("lugui")}}>Change the name</button> */}
