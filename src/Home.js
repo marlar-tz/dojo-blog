157e340484fb2c0e1e81aba0ee095b9a8a3d3b7e
marlar-tz <marlarhal55@gmail.com>
2025-10-13 23:56:10 +0800

    Making a custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,15 @@
-import { useState , useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './usefetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending , setIsPending] = useState(true);
 
-    const [error , setError] = useState(null);
-    useEffect(()=>{
-        setTimeout(()=>{
-            fetch('http://localhost:8080/blogss')
-        .then(res =>{
-            if(!res.ok){
-                throw Error('could not fetch the data for that resources');
-            }
-           
-            return res.json();
-        })
-        .then((data)=>{
-            setBlogs(data);
-            setIsPending(false);
-            setError(null);
-        })
-        .catch(err => {
-            setError(err.message);
-            setIsPending(false);
-        })
-        },1000);
-    },[])
+    const { data, isPending, error } = useFetch('http://localhost:8080/blogs');
 
     return (
         <div className="home">
             {error && <div>{error}</div>}
-            { isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All blogs!" />}
+            {isPending && <div>Loading...</div>}
+            {data && <BlogList blogs={data} title="All blogs!" />}
             {/* <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario' )} title="Mario's blogs!" /> */}
             {/* <button onClick={()=>{setName("lugui")}}>Change the name</button> */}
         </div>
