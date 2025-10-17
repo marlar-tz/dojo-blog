83383e6c70bfe13589ba7fbd7838bf8c6330d78b
marlar-tz <marlarhal55@gmail.com>
2025-10-17 12:33:00 +0800

    Added Routes parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 const BlogList = (props) => {
 
     const blogs = props.blogs;
@@ -8,8 +10,11 @@ const BlogList = (props) => {
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
+                    <Link to={`/blogs/${blog.id}`}>
                     <h2>{blog.title}</h2>
                     <p>Written byt {blog.author}</p>
+
+                    </Link>
                 </div>
             ))}
         </div>
